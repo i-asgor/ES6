2ad14cfc61be37e50a68bcaf00be484f9446e359
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-02-24 00:36:30 +0600

    let & const

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,10 @@ console.log(`My name is ${name} and my age is ${age} years old. I am ${age<18?'n
 console.log(name.padStart(15,'*'))
 console.log(name.padEnd(15,'a'))
 console.log('S'.repeat(10))
+
+let a=10
+a = 100
+console.log(a)
+
+const b=10
+console.log(b)
