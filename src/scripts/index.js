20bccdcf461a86187b04f7feef016e0228bea600
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-02-24 00:27:23 +0600

    string literals & few method

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,7 @@ var age= 30
 var name = "Md. Asgor Ali"
 console.log('my name is '+name + ' and  my age is '+age+' years old')
 console.log(`My name is ${name} and my age is ${age} years old. I am ${age<18?'not ':''} adult`)
+
+console.log(name.padStart(15,'*'))
+console.log(name.padEnd(15,'a'))
+console.log('S'.repeat(10))
